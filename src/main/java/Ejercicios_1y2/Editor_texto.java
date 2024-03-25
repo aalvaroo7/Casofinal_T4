e92e3931eaa876f5a9ffc310a5acd334de546be2
aalvaroo7 <almarrom@myuax.com>
2024-03-25 13:57:24 +0100

    Un recuerdo leve de esto que siento

--- src/main/java/Ejercicios_1y2/Editor_texto.java
+++ src/main/java/Ejercicios_1y2/Editor_texto.java
@@ -1,11 +1,10 @@
+package Ejercicios_1y2;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
