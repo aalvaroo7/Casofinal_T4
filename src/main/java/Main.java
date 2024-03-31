718f2b6339dc845853464363e187154975dc12d2
aalvaroo7 <almarrom@myuax.com>
2024-03-31 15:06:34 +0200

    asi si

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
+import contactos.Contacto;
+import contactos.Contacto;
+import contactos.Gestorcontactos;
+
 public class Main {
+    public static void main(String[] args) {
+        Gestorcontactos gestorContactos = new Gestorcontactos();
+
+        // Agregar un contacto
+        Contacto contacto = new Contacto("Nombre", "Correo", "Telefono");
+        gestorContactos.agregarContacto(contacto);
+
+        // Eliminar un contacto
+        gestorContactos.eliminarContacto(contacto);
+
+        // Buscar un contacto por nombre
+        Contacto contactoBuscado = gestorContactos.buscarContactoPorNombre("Nombre");
+        if (contactoBuscado != null) {
+            System.out.println("Contacto encontrado: " + contactoBuscado.getNombre());
+        } else {
+            System.out.println("Contacto no encontrado");
+        }
+    }
 }
